fabb32eca8dbe5cfcd439d5bc075ddc2525aaeb3
эмиль ахметханов <emil.ah@yandex.ru>
2022-11-01 21:03:44 +0300

    Добавьте файлы проекта.

---
+++ Main.cs
@@ -0,0 +1,37 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPITrainingRibbon
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Main : IExternalApplication
+    {
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+        }
+
+        public Result OnStartup(UIControlledApplication application)
+        {
+            string tabName = "Revit API training";
+            application.CreateRibbonTab(tabName);
+            string utilsFolderPath = @"C:\Program Files\RevitAPITraining\";
+
+            var panel = application.CreateRibbonPanel(tabName, "Трубы");
+
+            var button = new PushButtonData("Система", "Смена системы труб", 
+                Path.Combine(utilsFolderPath, "RevitAPITrainingUI.dll"),
+                "RevitAPITrainingUI.Main");
+            panel.AddItem(button);
+
+            return Result.Succeeded;
+        }
+    }
+}
